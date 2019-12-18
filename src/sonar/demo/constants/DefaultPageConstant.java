cc1c9ccc545eec65c19569bc28b634a735460b9f
rohmalho <58813147+rohmalho@users.noreply.github.com>
2019-12-18 09:14:59 +0530

    patch-1 commit

---
+++ src/sonar/demo/constants/DefaultPageConstant.java
@@ -0,0 +1,15 @@
+package sonar.demo.constants;
+
+import sonar.demo.service.DefaultPageService;
+
+public class DefaultPageConstant {
+	
+	public void getDefaultPage() {
+		DefaultPageService.getDefaultPage();
+		System.out.print("dsddssd");
+		String test = null;
+		System.out.print(test.length());
+		//goto:Statement1;
+	}
+
+}
